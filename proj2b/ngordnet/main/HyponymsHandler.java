e15ddb55722d66a7270ea8ab4b539602f27c2802
Chengming Li <chengming.li@berkeley.edu>
2023-03-21 15:46:53 -0700

    Fixed style

--- proj2b/ngordnet/main/HyponymsHandler.java
+++ proj2b/ngordnet/main/HyponymsHandler.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Collections;
 
 public class HyponymsHandler extends NgordnetQueryHandler {
